a6ae9ac1a56ca4d2625c2e21a16d4bf8d4238116
sumithgs <sumithgs2000@gmail.com>
2026-01-28 19:49:28 -0500

    Basic setup

---
+++ config/paths_config.py
@@ -0,0 +1,12 @@
+import os
+
+# Data ingestion
+
+RAW_DIR = "artifacts/raw"
+RAW_FILE_PATH = os.path.join(RAW_DIR,"raw.csv")
+
+TRAIN_FILE_PATH = os.path.join(RAW_DIR,"train.csv")
+TEST_FILE_PATH = os.path.join(RAW_DIR,"test.csv")
+
+CONFIG_PATH = "config/config.yaml"
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="hotel_reserv",
+    version="0.0.1",
+    author="Sumith",
+    packages=find_packages(),
+    install_requires=requirements,
+)
